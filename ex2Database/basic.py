c7da5e420184f42faa3e02b2d992dbf6525ceb90
Dharmesh-Poddar <dharmeshpoddar3@gmail.com>
2019-12-18 22:06:53 +0530

    database part

---
+++ ex2Database/basic.py
@@ -0,0 +1,15 @@
+import os
+from flask import Flask 
+from flask_sqlalchemy import flask_sqlalchemy
+
+basedir =os.path.abspath(os.path.dirname(__file__))
+
+app=Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///'+os.path.join(basedir,'data.sqlite')
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+
+db=SQLAlchemy(app)
+
+
