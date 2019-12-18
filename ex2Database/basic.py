958aa53bc96fa43fd1a05f86a0b5847c4dedf3c2
Dharmesh-Poddar <dharmeshpoddar3@gmail.com>
2019-12-18 22:27:44 +0530

    database

--- ex2Database/basic.py
+++ ex2Database/basic.py
@@ -13,3 +13,21 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 db=SQLAlchemy(app)
 
 
+##################### Model h bhai h aage########
+
+class puppy(db.Model):
+
+	__tablename__='puppies'
+
+	id =db.Column(db.Integer,primary_key=True)
+    name= db.Column(db.Text)
+    age= db.Column(db.Integer)
+
+    def __init__(self,name,age):
+          self.name =name 
+          self.age = age
+
+    def __repr__(self):
+           return f"Puppy {self.name} is {self.age} years old"
+
+           
