b0abb8c3eda43b23cb75bf6217f83a50610812c9
Dharmesh-Poddar <dharmeshpoddar3@gmail.com>
2019-12-23 22:11:01 +0530

    init file

--- LoginMechanism/myproject/models.py
+++ LoginMechanism/myproject/models.py
@@ -15,4 +15,8 @@ class User(db.Model,UserMixin):
        self.username=username
        self.password_hash= generate_password_hash(password)
     
+
+     def check_password(self,password)
+        return check_password_hash(self.password_hash,password)
+
     
