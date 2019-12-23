aea4ea5ecc6179435a3253227a5cfc8d34e8c38a
Dharmesh-Poddar <dharmeshpoddar3@gmail.com>
2019-12-23 23:34:06 +0530

    forms file

--- LoginMechanism/myproject/forms.py
+++ LoginMechanism/myproject/forms.py
@@ -12,4 +12,14 @@ class RegisterationForm(FlaskForm):
 	 pass_confirm=PasswordField('Confirm Password',validators=[DataRequired()])
 	 submit =SubmitField('Register!')
 
-	 
+	 def check_email(self,field):
+	 	 if User.query.filter_by(email=field.data).first():
+	 	      raise ValidationError('Your email has been already registered')
+
+     def check_username(self,field):
+          if User.query.filter_by(username=field.data).first():
+               raise ValidationError('Username is taken!')
+
+
+               
+       
