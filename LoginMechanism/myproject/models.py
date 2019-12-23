7c796e674eea1ec9425b7c639f2c8a7914619cf6
Dharmesh-Poddar <dharmeshpoddar3@gmail.com>
2019-12-23 22:08:49 +0530

    init file

--- LoginMechanism/myproject/models.py
+++ LoginMechanism/myproject/models.py
@@ -4,4 +4,15 @@ from flask_login import UserMixin
 
 class User(db.Model,UserMixin):
 	__tablename__='users'
-	
+
+	id=db.Column(db.Integer,primary_key=True)
+	email= db.Column(db.String(64),uniqu=True,index=True)
+    username= db.Column(db.String(64),unique=True,index=True)
+    password_hash= db.Column(db.String(128))
+    
+    def __init__(self,email,username,password):
+       self.email=email
+       self.username=username
+       self.password_hash= generate_password_hash(password)
+    
+    
