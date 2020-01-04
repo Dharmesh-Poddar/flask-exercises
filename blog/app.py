95b58fcab3bded47dae5722a81885b7112a10c11
Dharmesh-Poddar <dharmeshpoddar3@gmail.com>
2020-01-04 22:39:50 +0530

    DATABASE PART

--- blog/app.py
+++ blog/app.py
@@ -1,5 +1,13 @@
 from flask import Flask,render_template
+from flask_sqlalchemy import SQL_Alchemy
+
+
+basedir =os.path.abspath(os.path.dirname(__file__))
 app =Flask(__name__)
+
+
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///'+os.path.join(basedir,'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 
 @app.route('/')
 def index():
