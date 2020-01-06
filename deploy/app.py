3b5aa349d0b7473e5d080072f6f2a1b6e2ff0285
Dharmesh-Poddar <dharmeshpoddar3@gmail.com>
2020-01-07 01:03:09 +0530

    new changes

--- deploy/app.py
+++ deploy/app.py
@@ -1,13 +1,12 @@
-from flask import flask
+from flask import Flask
 
 app=Flask(__name__)
 
 
 @app.route('/')
-def index:
-	return hello goals
+def index():
+	return "hello goals"
 
 
 if __name__=='__main__':
 	app.run(debug='True')
-	
