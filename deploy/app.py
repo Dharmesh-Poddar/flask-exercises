be530524f93b23f5afaf2fbbda8e401550b486a8
Dharmesh-Poddar <dharmeshpoddar3@gmail.com>
2020-01-07 23:39:02 +0530

    added some new changes to stripe doc

--- deploy/app.py
+++ deploy/app.py
@@ -1,12 +1,12 @@
-from flask import Flask
+import os
+from flask import Flask, render_template, request
+import stripe
 
-app=Flask(__name__)
+stripe_keys = {
+  'secret_key': os.environ['SECRET_KEY'],
+  'publishable_key': os.environ['PUBLISHABLE_KEY']
+}
 
+stripe.api_key = stripe_keys['secret_key']
 
-@app.route('/')
-def index():
-	return "hello goals"
-
-
-if __name__=='__main__':
-	app.run(debug='True')
+app = Flask(__name__)
