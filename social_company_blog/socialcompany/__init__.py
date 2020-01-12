5a34590b39d77d2501d9a03c3ce4b1ad3ba9fe41
Dharmesh-Poddar <dharmeshpoddar3@gmail.com>
2020-01-12 17:32:31 +0530

    error pages

--- social_company_blog/socialcompany/__init__.py
+++ social_company_blog/socialcompany/__init__.py
@@ -3,5 +3,7 @@ from flask import Flask
 app = Flask(__name__)
 
 from socialcompany.core.views import core
+from socialcompany.error_pages.handlers import error_pages
 
 app.register_blueprint(core)
+app.register_blueprint(error_pages)
