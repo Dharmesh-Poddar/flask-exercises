cb3097b2d08db20f6ae2528cbaafca92423d1978
Dharmesh-Poddar <dharmeshpoddar3@gmail.com>
2020-01-13 01:37:07 +0530

    database part

--- social_company_blog/socialcompany/__init__.py
+++ social_company_blog/socialcompany/__init__.py
@@ -1,6 +1,24 @@
+import os
 from flask import Flask 
+from flask_sqlalchemy import Flask_SQLAlchemy 
+from flask_migrate import Migrate 
+from flask_login import LoginManager
+
 
 app = Flask(__name__)
+
+############################
+######Database part##########
+#############################
+
+basedir =os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///'+os.path.join(basedir,'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False 
+
+
+db=SQLAlchemy(app)
+Migrate(app,db)
+
 
 from socialcompany.core.views import core
 from socialcompany.error_pages.handlers import error_pages
