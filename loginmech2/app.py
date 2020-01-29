fc7616a04ddc1fa39fc64c6f13630bc52be617c8
Dharmesh-Poddar <dharmeshpoddar3@gmail.com>
2020-01-29 11:20:06 +0530

    forms

--- loginmech2/app.py
+++ loginmech2/app.py
@@ -6,7 +6,20 @@ from wtforms.validator import InputRequired,Email,Length
 
 
 app=flask(__name__)
+
+app.config['SECRET_KEY']='thisisthesecretkey'
+
 Bootstrap(app)
+
+class LoginForm(FlaskForm):
+	username = StringField('username', validators=[InputRequired(), Length(min=4,max=15)])
+    password = PasswordField('password', validators=[InputRequired(), Length(min=8,max=80)])
+    remember = BooleanField('remember me')
+
+class RegisterForm(FlaskForm): 
+	email = StringField('email', validators=[InputRequired(), Email(message='Invalid email'), Length(max=50)])
+	username = StringField('username', validators=[InputRequired(),Length(min=4, max=15)])
+    password = PasswordField('password', validators=[InputRequired(),Length(min=8, max=80)])
 
 @app.route('/')
 def index():
@@ -14,6 +27,7 @@ def index():
 
 @app.route('/login')
 def login():
+	form=LoginForm()
 	return render_template('login.html')
 
 @app.route('/signup')
