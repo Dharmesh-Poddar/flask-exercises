70f86fa52674022d9291e268832886504ed777dd
Dharmesh-Poddar <dharmeshpoddar3@gmail.com>
2020-01-31 01:23:04 +0530

    chat app

--- loginmech2/app.py
+++ loginmech2/app.py
@@ -1,43 +1,25 @@
-from flask import Flask,render_template
-from flask_wtf import FlaskForm
-from flask_bootstrap import Bootstrap 
-from wtforms import StringField,PasswordField,BooleanField
-from wtforms.validator import InputRequired,Email,Length
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 
-
-app=flask(__name__)
-
-app.config['SECRET_KEY']='thisisthesecretkey'
-
+app = Flask(__name__)
 Bootstrap(app)
-
-class LoginForm(FlaskForm):
-	username = StringField('username', validators=[InputRequired(), Length(min=4,max=15)])
-    password = PasswordField('password', validators=[InputRequired(), Length(min=8,max=80)])
-    remember = BooleanField('remember me')
-
-class RegisterForm(FlaskForm): 
-	email = StringField('email', validators=[InputRequired(), Email(message='Invalid email'), Length(max=50)])
-	username = StringField('username', validators=[InputRequired(),Length(min=4, max=15)])
-    password = PasswordField('password', validators=[InputRequired(),Length(min=8, max=80)])
 
 @app.route('/')
 def index():
-	return render_template('index.html')
+    return render_template('index.html')
 
 @app.route('/login')
 def login():
-	form=LoginForm()
-	return render_template('login.html')
+    return render_template('login.html')
 
 @app.route('/signup')
 def signup():
-	return render_template('signup.html')
+    return render_template('signup.html')
 
 @app.route('/dashboard')
 def dashboard():
-	return render_template('dashboard.html')
+    return render_template('dashboard.html')
 
-
-if __name__=='main':
-	app.run(debug='True')
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
