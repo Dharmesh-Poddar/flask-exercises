5ab28b00f4ae08d8a5acaf69e51da9b6982f8e46
Dharmesh-Poddar <dharmeshpoddar3@gmail.com>
2020-02-08 20:05:06 +0530

    database extenion

--- filesDatabase/app.py
+++ filesDatabase/app.py
@@ -1,5 +1,19 @@
 from flask import Flask,render_template,request
+from flask_sqlalchemy import FLASK_SQLAlchemy 
+
 app=Flask(__name__)
+basedir= os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///'+os.path.join(basedir,'data.sqlite')
+db= SQLAlchemy(app)
+
+class addfiles(db.Model):
+	 id= db.Column(db.Integer,primary_key='True')
+	 name=db.Column(db.string(300))
+     data= db.Column(db.LargeBinary)
+
+
+
+
 
 @app.route('/')
 def index():
@@ -8,7 +22,11 @@ def index():
 @app.route('/upload',methods=['POST'])
 def upload():
 	file= request.files['inputfile']
-	return file.filename
+	  
+	myfile= addfiles(name=inputfile,data=file)
+    db.session.add()
+    db.session.commit()    
+	return "file got added successfully" 
 
 if __name__=='main':
 	app.run(debug='True')
