65ff24e0e6d6d382ddb89c1cb087723b335c80fc
Dharmesh-Poddar <dharmeshpoddar3@gmail.com>
2020-05-01 17:31:44 +0530

    defining the guidelines

--- AccessingPdf/app.py
+++ AccessingPdf/app.py
@@ -1,3 +1,18 @@
+import os
 from flask import Flask,request
 import requests
 from flask_sqlalchemy import SQLAlchemy 
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']= 'sqlite:///'+os.path.join(basedir,'data.sqlite')
+
+db= SQLAlchemy(app)
+
+
+
+
+
+if __name__=='__main__':
+	app.run(debug=True)
+	
